d94a6631938e792ad848377faba349049705d954
Voltaire Ignacio <volferign27@gmail.com>
2019-05-28 19:17:00 +0800

    Init Commit

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+console.log('client side javascript file is loaded!')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2') 
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+
+    fetch('http://localhost:3000/weather?location=' + search.value).then((response) => {
+        response.json().then((data) => {
+            if (data.error){
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast 
+            }
+        })
+    })
+})
---
+++ src/app.js
@@ -0,0 +1,102 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname, '../templates/views')
+const partialPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+hbs.registerPartials(partialPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Voltaire'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Voltaire'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        name: 'Voltaire'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.location){
+        return res.send({
+            error: 'You must provide location'
+        })
+    }
+
+    geocode(req.query.location, (error, { latitude, longitude, location } = {}) => {
+        if (error){
+            return res.send({error})
+        }
+    
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error){
+                return res.send({error})
+            }
+    
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.location
+            })            
+          })    
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Voltaire Ignacio',
+        errorMessage: 'Help not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Voltaire Ignacio',
+        errorMessage: 'Page not found.'
+    })
+})
+// app.com
+// app.com/help
+// app.com/about
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
